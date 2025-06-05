00a94de96877063c97b6eb82530e98e5f9a6555b
Agung Fathul Muhtadin <90741091+AgungFathul01@users.noreply.github.com>
2025-06-05 07:07:47 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Pinad Calculator',
+  description: 'Created by agungfathul',
 }
 
 export default function RootLayout({
